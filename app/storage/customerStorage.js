393b90bcf3e96188cc08f4d641267b7f1565115b
Maxim Rybka <maxim.rybka@sensoramalab.com>
2020-04-12 21:41:47 +0300

    conn release added

--- app/storage/customerStorage.js
+++ app/storage/customerStorage.js
@@ -3,19 +3,25 @@ const db = require('./dbexec');
 async function getAllCustomers() {
     // получение объектов
     const conn = await db.connection();
-    return conn.query("SELECT cust_edrpou, cust_itn, cust_phone, cust_region, cust_city, cust_street, cust_building, cust_email, cust_acc, cust_debt, cust_notes FROM customer");
+    let res = await conn.query("SELECT cust_edrpou, cust_itn, cust_phone, cust_region, cust_city, cust_street, cust_building, cust_email, cust_acc, cust_debt, cust_notes FROM customer");
+    conn.release();
+    return res;
 }
 
 async function getCustomer(id){
     // получение объектов
     const conn = await db.connection();
-    return conn.query(`SELECT cust_edrpou, cust_itn, cust_phone, cust_region, cust_city, cust_street, cust_building, cust_email, cust_acc, cust_debt, cust_notes FROM customer WHERE cust_edrpou= ${id}`);
+    let res = await conn.query(`SELECT cust_edrpou, cust_itn, cust_phone, cust_region, cust_city, cust_street, cust_building, cust_email, cust_acc, cust_debt, cust_notes FROM customer WHERE cust_edrpou= ${id}`);
+    conn.release();
+    return res;
 }
 
 async function getCustomerOrders(id){
     // получение объектов
     const conn = await db.connection();
-    return conn.query(`SELECT ord_id, ord_date, ord_notes ,O.cust_edrpou FROM mydb.order O INNER JOIN customer C ON O.cust_edrpou = C.cust_edrpou WHERE C.cust_edrpou = ${id};`);
+    let res = await conn.query(`SELECT ord_id, ord_date, ord_notes ,O.cust_edrpou FROM mydb.order O INNER JOIN customer C ON O.cust_edrpou = C.cust_edrpou WHERE C.cust_edrpou = ${id};`);
+    conn.release();
+    return res;
 }
 
 module.exports = { getAllCustomers , getCustomer,getCustomerOrders };
--- app/storage/orderStorage.js
+++ app/storage/orderStorage.js
@@ -3,13 +3,17 @@ const db = require('./dbexec');
 async function getAllOrders() {
     // получение объектов
     const conn = await db.connection();
-    return conn.query("SELECT ord_id, ord_date, ord_notes , cust_edrpou FROM mydb.order;");
+    let res= await conn.query("SELECT ord_id, ord_date, ord_notes , cust_edrpou FROM mydb.order;");
+    conn.release();
+    return res;
 }
 
 async function getOrder(id){
     // получение объектов
     const conn = await db.connection();
-    return conn.query(`SELECT ord_id, ord_date, ord_notes , cust_edrpou FROM mydb.order WHERE ord_id = ${id};`);
+    let res= await conn.query(`SELECT ord_id, ord_date, ord_notes , cust_edrpou FROM mydb.order WHERE ord_id = ${id};`);
+    conn.release();
+    return res;
 }
 
 module.exports = { getAllOrders , getOrder };

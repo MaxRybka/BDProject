e7505d195db0f6b6b6e721d2935e9eb4ef5b4277
Nikita <vedenikovnekit@gmail.com>
2020-04-16 01:22:06 +0300

    creted manufacturers, categories on front

---
+++ src/modules/manuf-html.js
@@ -0,0 +1,29 @@
+let _makeHtml = ({
+	man_id,
+	man_name,
+	man_phone,
+	man_email,
+	man_city,
+	man_street,
+	man_building,
+	man_notes
+}) => {
+	let $product = $(`<tr data-manomer-id="${man_id}"> 
+      <th scope="row" >${man_id}</th>
+      <td>${man_name}</td>
+      <td>${man_phone}</td>
+      <td>${man_email}</td>
+      <td>${man_city}</td>
+      <td>${man_street}, ${man_building}</td>`);
+
+     if(man_notes != null) {
+	    $($product).append(`<td class="text-break">${man_notes}</td>`);
+	   }else{
+	   	$($product).append(`<td>-</td>`);
+
+	   	};
+       $($product).append(`<td> <button type="button" id ="delmanbtn" class="btn btn-outline-danger butmar">Delete</button></td>
+    </tr>`);
+	return $product;
+};
+ module.exports = _makeHtml;

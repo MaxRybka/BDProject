fdd66de0596715b421c88997627839158acbba54
Ihor Paprotskyi <igorpapr@gmail.com>
2020-04-15 22:42:00 +0300

    added bat price batinv

--- app/storage/batchStorage.js
+++ app/storage/batchStorage.js
@@ -3,7 +3,7 @@ const db = require('./dbexec');
 async function getBatchesByInvoiceId(invId) {
     // получение объектов
     const conn = await db.connection();
-    let res = await conn.query(`SELECT B.prod_cd, P.prod_name, B.bat_extra, B.bat_amount, B.bat_initamount, B.bat_endprice, B.bat_lineprice, P.man_id, M.man_name
+    let res = await conn.query(`SELECT B.prod_cd, P.prod_name, B.bat_extra, B.bat_price, B.bat_amount, B.bat_initamount, B.bat_endprice, B.bat_lineprice, P.man_id, M.man_name
                                  FROM batch B INNER JOIN product P ON B.prod_cd = P.prod_cd
                                               INNER JOIN manufacturer M ON P.man_id = M.man_id 
                                  WHERE B.inv_id = ${invId};`);

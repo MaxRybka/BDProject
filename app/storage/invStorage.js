e6de4d40166a53aeb71b2b6072f42a805f5efca2
Ihor Paprotskyi <igorpapr@gmail.com>
2020-04-16 13:52:35 +0300

    small fix

--- app/storage/invStorage.js
+++ app/storage/invStorage.js
@@ -12,10 +12,11 @@ async function addNewInvoice(invData, batchesArr) {
     const conn = await db.connection();
     try {
         await conn.beginTransaction();
-        console.log('Inv data: ' + `${invData}`);
-        console.log('Batches arr: ' + `${batchesArr}`);
-        await conn.query('INSERT INTO invoice (inv_id, inv_date, inv_notes, sup_edrpou) VALUES (?, ?, ?, ?)', invData);
+        //console.log('Inv data: ' + `${invData}`);
+        console.log('Batches arr: ');
+        console.log(batchesArr);
 
+        await conn.query('INSERT INTO invoice (inv_id, inv_date, inv_notes, sup_edrpou) VALUES (?, ?, ?, ?)', invData);
         let batchsql = 'INSERT INTO batch (prod_cd, inv_id, bat_extra, bat_amount, bat_price, bat_initamount) VALUES ';
 
         for (let i = 0; i < batchesArr.length; i++) {
@@ -25,8 +26,7 @@ async function addNewInvoice(invData, batchesArr) {
             }
         }
         batchsql += ';';
-        console.log('sosiska');
-        console.log('Batch sql: ' + `${batchsql}`);
+        //console.log('Batch sql: ' + `${batchsql}`);
         await conn.query(batchsql);
 
         for (let prod of batchesArr) {
@@ -34,7 +34,6 @@ async function addNewInvoice(invData, batchesArr) {
         }
         await conn.commit();
         console.log("Successfully added new invoice and batches. COMMITTED...");
-
     } catch (e) {
         console.log("Error occured while adding new invoice.\nROLLBACK...\n: " + e.stack);
         await connection.rollback();

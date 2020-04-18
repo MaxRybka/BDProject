50c4055e2da4ed191743759529d0217aacc3c345
Maxim Rybka <maxim.rybka@sensoramalab.com>
2020-04-18 17:51:21 +0300

    login created

--- server.js
+++ server.js
@@ -13,6 +13,32 @@ let order = require('./app/order');
 let manuf = require('./app/manuf');
 let categ = require('./app/category');
 
+//Session 
+const passwordHash = require('password-hash');
+const redis = require('redis');
+const session = require('express-session');
+const redisStorage = require('connect-redis')(session);
+const client = redis.createClient();
+//session duration
+const session_duration = 120;
+
+let userDao = require('./app/storage/userStorage');
+
+
+server.use(
+    session({
+        store: new redisStorage({
+            host: '127.0.0.1',
+            port: 6379,
+            client: client,
+            ttl: session_duration
+          }),
+        secret: 'secret',
+        resave: false,
+        saveUninitialized: true
+    })
+  )
+
 server.listen(8888);
 console.log('Server is running on port 8888');
 server.use(express.static(__dirname));
@@ -44,31 +70,48 @@ server.get('/', function(req, res) {
 
 });
 
-//Login
-server.get('/login', function(req, res) {
-    //TODO - check token + session 
+//Login 
+//if session expired redirect here
+server.post('/login',jsonParser, function(req, res) {
+    console.log(req.body);
+    var login = req.body.login;
+    var password = req.body.password;
 
-    res.write("Login page here")
-    res.end();
-    //res.sendFile(__dirname+"");
+    if(login && password){
+
+        //get user info from db
+        userDao.getUserByLogin(login).then((data)=>{
+            let userData = data[0][0];
+            if(data[0].length !== 0 &&  passwordHash.verify(password,userData.pass)){
+                //user exists
+                if (!req.session.accessToken) 
+                    req.session.accessToken = req.sessionID
+                
+
+                req.session.loggedin = true;
+                req.session.test_login = login;
+                req.session.role = userData.role;
+                req.session.maxAge = session_duration;
+
+                res.end();
+            }else{
+                //throw err
+                res.status(400).send({ "err": "Incorrect Username and/or Password!" });
+            }
+        }).catch(err => {
+            res.write(err.stack);
+            res.end();
+        });
+    }else{
+        //Redirect to login + notify
+    }
 });
-
-//???????? - login (POST?)
 
 //Admin
 server.get('/admin', function(req, res) {
     //TODO - check token + session 
 
     res.write("Admin page here")
-    res.end();
-    //res.sendFile(__dirname+"");
-});
-
-//Admin login
-server.get('/admin/login', function(req, res) {
-    //TODO - check token + session 
-
-    res.write("Admin login page here")
     res.end();
     //res.sendFile(__dirname+"");
 });
